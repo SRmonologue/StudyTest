565904f69d93a1eb0ace64354854770bb6d6e687
guozhen <424361609@qq.com>
2017-11-16 13:57:01 +0800

    View

--- app/src/main/java/com/example/myapplication/activity/Test6Activity.java
+++ app/src/main/java/com/example/myapplication/activity/Test6Activity.java
@@ -3,6 +3,15 @@ package com.example.myapplication.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.myapplication.R;
 
@@ -13,19 +22,100 @@ import com.example.myapplication.R;
 public class Test6Activity extends AppCompatActivity {
 
 
+    private ViewGroup mAnim_mask_layout;
+    private ImageView mShow;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test6);
+        ImageView add = (ImageView) findViewById(R.id.image_add);
+        mShow = (ImageView) findViewById(R.id.image_show);
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] start_location = new int[2];
+                //起始点下X,Y
+                v.getLocationInWindow(start_location);
+                ImageView imageView = new ImageView(Test6Activity.this);
+                imageView.setBackground(getDrawable(R.mipmap.ic_launcher));
+                //开始执行动画
+                startAnima(imageView, start_location);
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private View addViewToAnimLayout(ViewGroup vg, View view, int[] location) {
+        int x = location[0];
+        int y = location[1];
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.leftMargin = x;
+        lp.topMargin = y;
+        view.setLayoutParams(lp);
+        return view;
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void startAnima(final ImageView imageView, int[] start_location) {
+        mAnim_mask_layout = createAnimLayout();
+        //把动画小球添加到动画层
+        mAnim_mask_layout.addView(imageView);
+        View view = addViewToAnimLayout(mAnim_mask_layout, imageView, start_location);
+        //动画结束时的坐标
+        int[] end_location = new int[2];
+        mShow.getLocationInWindow(end_location);
+
+        //计算位移
+        int endX = 0 - start_location[0] + 40;
+        int endY = end_location[1] - start_location[1];
+        TranslateAnimation translateAnimationX = new TranslateAnimation(0, endX, 0, 0);
+        translateAnimationX.setInterpolator(new LinearInterpolator());
+        translateAnimationX.setFillAfter(true);
+
+        TranslateAnimation translateAnimationY = new TranslateAnimation(0, 0, 0, endY);
+        translateAnimationY.setInterpolator(new AccelerateInterpolator());
+        translateAnimationY.setFillAfter(false);
+
+        AnimationSet set = new AnimationSet(false);
+        set.setFillAfter(false);
+        set.addAnimation(translateAnimationX);
+        set.addAnimation(translateAnimationY);
+        set.setDuration(800);
+        view.startAnimation(set);
+
+        set.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                imageView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    /**
+     * 创建动画层
+     */
+
+    private ViewGroup createAnimLayout() {
+        ViewGroup rootView = (ViewGroup) getWindow().getDecorView();
+        LinearLayout animLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        animLayout.setLayoutParams(lp);
+        animLayout.setId(Integer.MAX_VALUE);
+        animLayout.setBackgroundResource(android.R.color.transparent);
+        rootView.addView(animLayout);
+        return animLayout;
     }
 }
