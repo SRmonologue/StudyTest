5002432dc9b544c0c9b14013b899877b9dae26d0
SRmonologue <424361609@qq.com>
2018-05-03 10:49:16 +0800

    补全提交

--- broadcastreceiver/src/main/java/com/wangban/broadcastreceiver/OpenMachineReceiver.java
+++ broadcastreceiver/src/main/java/com/wangban/broadcastreceiver/OpenMachineReceiver.java
@@ -3,7 +3,6 @@ package com.wangban.broadcastreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 /**
@@ -13,9 +12,6 @@ public class OpenMachineReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle("我就试试看！");
-        builder.show();
         Toast.makeText(context,"我开机了你知道吗?",Toast.LENGTH_SHORT).show();
     }
 }
